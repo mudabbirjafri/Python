6639058f128f7b3fb7fba2cacc96d2f4dac4fd1d
mudabbirjafri <mudabbir.j@gmail.com>
2019-06-01 13:44:41 -0700

    Added More preprocessing and some more libs

--- nyc-propertysales.py
+++ nyc-propertysales.py
@@ -3,10 +3,29 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 import pandas as pd
-from sklearn import datasets, svm, tree, preprocessing, metrics
-import sklearn.ensemble as ske
 import os
 import datetime
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet
+
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+
+from scipy import stats
+
 
 os.chdir('C:/Users/Mudabbir/Desktop/Spring Quarter/Machine Learning 808/nyc-property-sales')
 nyc_data = pd.read_csv('nyc-rolling-sales.csv')
@@ -14,7 +33,8 @@ nyc_data = pd.read_csv('nyc-rolling-sales.csv')
 # examine the dataset
 nyc_data.dtypes
 nyc_data.shape
-nyc_data.isna().sum()
+nyc_data.isnull().sum()
+
 nyc_data.groupby(['TAX CLASS AT PRESENT']).count().iloc[:,:1]/len(nyc_data)
 nyc_data.groupby(['BOROUGH']).count().iloc[:,:1]/len(nyc_data)
 
@@ -23,13 +43,27 @@ nyc_data.head()
 # preprocessing
 nyc_data['SALE DATE'] = pd.to_datetime(nyc_data["SALE DATE"])
 nyc_data = nyc_data.replace(to_replace=' -  ', value ='0')
-nyc_data['SALE PRICE'] = nyc_data['SALE PRICE'].astype('int64')
+nyc_data['SALE PRICE'] = pd.to_numeric(nyc_data['SALE PRICE'], errors='coerce')
+nyc_data['BOROUGH'] = nyc_data['BOROUGH'].astype('category')
 nyc_data['BUILDING CLASS AT TIME OF SALE']=pd.Categorical(nyc_data['BUILDING CLASS AT TIME OF SALE']).codes
 nyc_data['BUILDING CLASS AT TIME OF SALE']=nyc_data['BUILDING CLASS AT TIME OF SALE'].astype('category').cat.codes
-
-
-nyc_data.drop(['EASE-MENT', 'APARTMENT NUMBER'], axis=1, inplace=True) #Remove Empty columns
+nyc_data['TAX CLASS AT TIME OF SALE'] = nyc_data['TAX CLASS AT TIME OF SALE'].astype('category')
+nyc_data['TAX CLASS AT PRESENT'] = nyc_data['TAX CLASS AT PRESENT'].astype('category')
+nyc_data['LAND SQUARE FEET'] = pd.to_numeric(nyc_data['LAND SQUARE FEET'], errors='coerce')
+nyc_data['GROSS SQUARE FEET']= pd.to_numeric(nyc_data['GROSS SQUARE FEET'], errors='coerce')
+nyc_data.drop(['EASE-MENT'], axis=1, inplace=True) #Remove Empty columns
 nyc_data =nyc_data.loc[:, ~nyc_data.columns.str.contains('^Unnamed')]
+sum(nyc_data.duplicated(nyc_data.columns))
+nyc_data = nyc_data.drop_duplicates(nyc_data.columns, keep='last')
+sum(nyc_data.duplicated(nyc_data.columns))
+
+#checking missing values
+nyc_data.columns[nyc_data.isnull().any()]
+miss=nyc_data.isnull().sum()/len(nyc_data)
+miss=miss[miss>0]
+miss.sort_values(inplace=True)
+print(miss) 
+
 
 
 #Use LabelEncoder to convert labels into binary codes
